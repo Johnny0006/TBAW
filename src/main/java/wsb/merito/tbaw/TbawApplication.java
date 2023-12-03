05f0e3f66e45ecdda69682852f0181db0abc787e
Johnny <jasiek0006@gmail.com>
2023-12-03 21:16:40 +0100

    init SpringBootApp

---
+++ src/main/java/wsb/merito/tbaw/TbawApplication.java
@@ -0,0 +1,13 @@
+package wsb.merito.tbaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/merito/tbaw/TbawApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.merito.tbaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
